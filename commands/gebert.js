e4773c3d35356edeb8a582c6217c94e19dc369c4
retriquaz <retriquaz@gmail.com>
2019-10-13 16:00:15 +0300

    Add files via upload

---
+++ commands/gebert.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args) => {
+  if(!message.content.startsWith("kb ")){
+      return;
+    }
+  else if(message.content === "kb gebert"){
+
+    message.reply("Birini Etiketlemeniz Lazım\nKullanım `kb gebert @TAG`")
+
+    } else {
+  const embed = new Discord.RichEmbed()
+    .setColor(0x800040)
+    .setImage(url='https://i.hizliresim.com/5Nzl4R.png')
+  message.channel.send("" + message.author.toString() + " :coffin: " + message.mentions.members.first() + "", embed);
+}
+}
