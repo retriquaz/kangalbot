745b21fdc1bfa9eb6c1b8a44e12382676d955da7
retriquaz <retriquaz@gmail.com>
2020-12-20 00:57:32 +0300

    Update wtf.js

--- commands/wtf.js
+++ commands/wtf.js
@@ -17,6 +17,6 @@ exports.run = (bot, message, args) => {
   const embed = new Discord.RichEmbed()
     .setColor(0xf79209)
     .setImage(url='https://cdn.discordapp.com/attachments/468301293581107201/500679873405124628/43952755_10157458613873492_7998499023902212096_n.png')
-  message.channel.send("" + message.mentions.members.first() + " Excuse me, WTF", embed);
+  message.channel.send("" + message.mentions.users.first() + " Excuse me, WTF", embed);
 }
 }
