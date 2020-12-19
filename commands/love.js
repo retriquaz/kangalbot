9df62341fa3efeecd8e1964ff5f607f6399d3228
retriquaz <retriquaz@gmail.com>
2020-12-20 00:56:46 +0300

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -17,6 +17,6 @@ exports.run = (bot, message, args) => {
   const embed = new Discord.RichEmbed()
     .setColor(0x800040)
     .setImage(url='https://image.ibb.co/fnNZg9/imloveu.jpg')
-  message.channel.send("" + message.author.toString() + " :heart: " + message.mentions.members.first() + "", embed);
+  message.channel.send("" + message.author.toString() + " :heart: " + message.mentions.users.first() + "", embed);
 }
 }
